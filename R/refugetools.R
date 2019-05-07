5eb5e42cf9cc2d02fefe59898cf838e6f216c2f7
McCrea Cobb <mccrea_cobb@fws.gov>
2019-05-07 08:46:03 -0800

    updated refugetools.R

--- R/refugetools.R
+++ R/refugetools.R
@@ -1,13 +1,12 @@
 #' refugetools: A package that contains helpful functions for planning and implementing refuge I&M surveys.
 #'
-#' The refugetools package contains three functions:
-#' create.dir, mdeditor, and dublin.
+#' A collection of tools for Alaska refuge biologists. It contains three functions: create.dir, mdeditor, and dublin.
 #'
-#' @section Create.dir function:
+#' @section create.dir function:
 #' The create.dir function creates a project working directory.
 #'
-#' @section Mdeditor function:
-#' The mdeditor function opens the mdEditor application for creating and editing data and project metadata (json files).
+#' @section mdeditor function:
+#' The mdeditor function opens the mdEditor application for creating and editing data and project metadata (json file format).
 #'
 #' @section dublin function:
 #'  The dublin function creates a metadata csv file to document the contents of the project working directory.
