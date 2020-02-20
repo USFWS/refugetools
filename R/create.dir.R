718620c2a67d7bbf3c4c14797ce68edee645f5ad
McCrea Cobb <mccrea_cobb@fws.gov>
2020-02-20 10:58:40 -0900

    update create.dir using Jared's edits

--- R/create.dir.R
+++ R/create.dir.R
@@ -1,65 +1,62 @@
 
-#' @name create.dir
-#' @title Creates a file directory for a new refuge I&M project
+#' Creates a File Directory for a New Refuge I&M Project
 #'
 #' @description A function to create a new file directory structure. The directory structure is based off the Alaska I&M biometric project directory template.
 #'
-#' @param dir Location to create new project folder.
-#' @param proj The name of the project, to be used as the name of the root folder.
+#' @param dir.name Location to create new root project folder. Default is the current working directory.
+#' @param proj.name The name of the project to be used as the name of the root project folder.
+#' @param moreFolders List or vector containing character strings specifying full directory paths for additional
+#' folders to be included in project directory.
 #'
-#' @examples create.dir(getwd(), "bear_survey")
+#' @examples create.dir(proj.name = "bear_survey")
 
-create.dir <- function(dir, proj) {
+create.dir <- function(proj.name = NULL, dir.name = NULL, moreFolders = NULL) {
 
-  # root folder
-  dir <- dir
-  setwd(dir)
+    ## project name
+    if(is.null(proj.name))proj.name = "myproject"
 
-  dir.create(proj)
+    ## root project folder
+    if(is.null(dir.name))dir.name = "."
+    root.dir = paste0(dir.name, "/", proj.name)
+    dir.create(root.dir)
 
-  dir.proj <- paste0(dir,"/", proj)
-  setwd(dir.proj)
+    ## primary project subfolders
+    f <- paste0(root.dir, "/", c("admin", "code", "data", "incoming", "metadata", "output", "products",
+                                 "protocols", "resources"))
+    lapply(f, dir.create)
 
-  f <- c("admin", "code", "data", "incoming", "output", "products",
-         "protocols", "resources")
-  lapply(f, dir.create)
+    ## /admin
+    f <- paste0(root.dir, "/admin/", c("contracts", "meetings", "proposals", "purchasing", "training", "travel"))
+    lapply(f, dir.create)
 
-  # /admin
-  setwd(paste0(dir.proj,"/admin"))
-  f <- c("contracts", "meetings", "proposals", "purchasing", "training", "travel")
-  lapply(f, dir.create)
+    ## /code
+    f <- paste0(root.dir, "/code/", c("functions"))
+    lapply(f, dir.create)
 
-  # /code
-  setwd(paste0(dir.proj,"/code"))
-  f <- c("functions")
-  lapply(f, dir.create)
+    ## /data
+    f <- paste0(root.dir, "/data/", c("derived", "raw"))
+    lapply(f, dir.create)
 
-  # /data
-  setwd(paste0(dir.proj,"/data"))
-  f <- c("derived", "raw")
-  lapply(f, dir.create)
+    ## /output
+    f <- paste0(root.dir, "/output/", c("figures", "raw_analysis", "tables"))
+    lapply(f, dir.create)
 
-  # /output
-  setwd(paste0(dir.proj,"/output"))
-  f <- c("figures", "raw_analysis", "tables")
-  lapply(f, dir.create)
+    ## /products
+    f <- paste0(root.dir, "/products/", c("conceptual_model", "posters", "presentations", "publications",
+                                          "reports"))
+    lapply(f, dir.create)
 
-  # /products
-  setwd(paste0(dir.proj,"/products"))
-  f <- c("conceptual_model", "posters", "presentations", "publications",
-         "reports")
-  lapply(f, dir.create)
+    ## /protocols
+    f <- paste0(root.dir, "/protocols/", c("draft_elements"))
+    lapply(f, dir.create)
 
-  # /protocols
-  setwd(paste0(dir.proj,"/protocols"))
-  f <- c("draft_elements")
-  lapply(f, dir.create)
+    ## /resources
+    f <- paste0(root.dir, "/resources/", c("data", "geodata", "images", "publications", "reports"))
+    lapply(f, dir.create)
 
-  # /resources
-  setwd(paste0(dir.proj,"/resources"))
-  f <- c("data", "geodata", "images", "publications", "reports")
-  lapply(f, dir.create)
+    f <- paste0(root.dir, "/resources/", c("data", "geodata", "images", "publications", "reports"))
+    lapply(f, dir.create)
 
-  setwd(dir)
-
+    ## user-provided additional folders to be created
+    if(!is.null(moreFolders))lapply(moreFolders, dir.create)
 }
